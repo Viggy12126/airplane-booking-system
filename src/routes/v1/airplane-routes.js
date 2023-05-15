f83fe0296f032e2d730a0dcf2a2fbd01fd78f23c
vighnesh <vighnesh2607@gmail.com>
2023-05-15 22:50:04 +0530

    added get all airplanes api

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -5,6 +5,7 @@ const { AirplaneController } = require('../../controllers');
 const{AirplaneMiddlewares}=require('../../middlewares');
 
 router.post('/', AirplaneMiddlewares.validateCreateRequest, AirplaneController.createAirplane);
+router.get('/',AirplaneController.getAirplanes);
 
 module.exports=router;
        
