9742f66ae93d8e16d3401cd19675c8a955edfc64
vighnesh <vighnesh2607@gmail.com>
2023-06-02 20:43:19 +0530

    added update city api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -25,6 +25,35 @@ async function createCity(req, res) {
     }
 }
 
+async function destroyCity(req,res) {
+
+    try {
+        const cities = await CityService.destroyCity(req.params.id);
+        // console.log(req.params.id);
+        SuccessResponse.data = cities;
+        return res.status(StatusCodes.OK).json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+
+}
+
+async function updateCity(req, res) {
+    try {
+      const cities = await CityService.updateCity(req.params.id, req.body);
+      SuccessResponse.data = cities;
+      return res.status(StatusCodes.OK).json(SuccessResponse);
+    } catch (error) {
+      ErrorResponse.error = error;
+      return res.status(error.statusCode).json(ErrorResponse);
+    }
+  }
+
 module.exports={
-    createCity
+    createCity,
+    destroyCity,
+    updateCity
 }
--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -6,5 +6,7 @@ const { CityController } = require('../../controllers');
 const router = express.Router();
 
 router.post('/',CityController.createCity);
+router.delete('/:id',CityController.destroyCity);
+router.patch('/:id',CityController.updateCity);
 
 module.exports=router;
