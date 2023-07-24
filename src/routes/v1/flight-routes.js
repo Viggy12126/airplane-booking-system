45003fbe494933046cab0b3b79c434a844197f89
vighnesh <vighnesh2607@gmail.com>
2023-07-24 18:16:44 +0530

    added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -10,6 +10,9 @@ router.post('/',
         FlightMiddlewares.validateCreateRequest,
         FlightController.createFlight);
 
-router.get('/',FlightController.getAllFlights);        
+router.get('/',FlightController.getAllFlights);  
+
+router.get('/:id', 
+        FlightController.getFlight);
 
 module.exports = router;
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -66,7 +66,20 @@ async function getAllFlights(query){
     }
 }
 
+async function getFlight(id) {
+    try {
+        const flight = await flightRepository.get(id);
+        return flight;
+    } catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The flight you requested is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of all the flight', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports = {
     createFlight,
-    getAllFlights
+    getAllFlights,
+    getFlight
 }
